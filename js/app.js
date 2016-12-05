bdb07d8cda48a1b0bc090c63188fd8a030720205
hectorguo <hectorguo@live.com>
2016-12-04 21:25:52 -0800

    add chat and sharedData

--- js/app.js
+++ js/app.js
@@ -11,6 +11,27 @@ var CLIENT_SECRET = '2P3itf8w1G5kgnY9';
 var TOKEN_STATE = 'lynking123';
 var SERVER_URL = 'https://4113studio.com';//'https://lynking-node.us-west-1.elasticbeanstalk.com'; // dev 'http://localhost:8080'
 
+nameApp.factory('sharedData', function() {
+  return {
+    profile: {
+      // emailAddress: '',
+      // firstName: '',
+      // formattedName: '',
+      // headline: '',
+      // industry: '',
+      // lastName: '',
+      // linkedinId: '',
+      // numConnections: '',
+      // pictureUrl: '',
+      // profileUrl: '',
+      // summary: '',
+      // location: []
+    },
+    friend: {},
+    matchedList: []
+  }
+})
+
 nameApp.directive("ngMobileClick", [function () {
     return function (scope, elem, attrs) {
         elem.bind("touchstart click", function (e) {
@@ -140,13 +161,18 @@ nameApp.config(function($stateProvider, $urlRouterProvider) {
       url: '/details',
       templateUrl: 'details.html',
       controller: 'DetailsCtrl'
+    })
+    .state('chat', {
+      url: '/chat/:linkedinId/:friendLinkedinId',
+      templateUrl: 'chat.html',
+      controller: 'ChatCtrl'
     });
  
   $urlRouterProvider.otherwise("/");
 
 });
 
-nameApp.controller('IndexCtrl', function($scope, $state) {
+nameApp.controller('IndexCtrl', function($scope, $state, sharedData) {
   $scope.redirect = function(){
     var linkedAuthUrl = 'https://www.linkedin.com/oauth/v2/authorization?response_type=code';
     linkedAuthUrl+= '&client_id='+CLIENT_ID;
@@ -177,6 +203,8 @@ nameApp.controller('IndexCtrl', function($scope, $state) {
       $state.go('search');
       profileLinkedinId = profile.linkedinId;
       profilePictureUrl = profile.pictureUrl;
+      profileName = profile.formattedName;
+      sharedData.profile = profile;
     });
   }
 
@@ -204,22 +232,32 @@ nameApp.controller('SearchCtrl', function($scope, $state, $ionicHistory) {
     });
 });
 
-nameApp.controller('CandidatesCtrl', function($scope, $http, $state, $ionicHistory, $ionicSlideBoxDelegate) {
+nameApp.controller('CandidatesCtrl', function($scope, $http, $state, $ionicHistory, $ionicSlideBoxDelegate, sharedData) {
   $scope.goBack = function(){
     $ionicHistory.goBack();
   }
   $scope.go = function(path) { 
     $location.path( path );
   }
+
+  var curProfile = sharedData.profile;
+
+  if(!curProfile.linkedinId) {
+    alert('Need login');
+    $state.go('index');
+    return;
+  }
   // fake data, get data from backend
   $.ajax({
     method: 'GET',
-    url: SERVER_URL+'/api/user/'+profileLinkedinId+'/match?distance=1500'
+    url: SERVER_URL+'/api/user/'+curProfile.linkedinId+'/match?distance=1500'
   }).then(function successCallback(response) {
     // console.log(response);
     // console.log(response.data[0].name);
     console.log(response.users);
     $scope.personsList = response.users;
+    sharedData.matchedList = response.users;
+
   }, function errorCallback(response) {
     console.log("data not get");
   });
@@ -254,20 +292,21 @@ nameApp.controller('CandidatesCtrl', function($scope, $http, $state, $ionicHisto
 
   // select this user
   document.getElementById("yeah-btn").onclick = function(){$scope.lookDetail();}
-  $scope.lookDetail = function(){
-    var tmpAvatarString = document.getElementsByClassName("swiper-slide-active")[0].getElementsByClassName("item-image")[0].getAttribute("style");
-    redirectAvatar = tmpAvatarString.substring(tmpAvatarString.indexOf("('")+2,tmpAvatarString.indexOf("')"));
-    redirectName = document.getElementsByClassName("swiper-slide-active")[0].getElementsByTagName('h2')[0].innerText;
-    redirectHeadLine = document.getElementsByClassName("swiper-slide-active")[0].getElementsByTagName('h3')[0].innerText;
-    redirectDistance = document.getElementsByClassName("swiper-slide-active")[0].getElementsByTagName('p')[0].innerText;
-    redirectSummary = document.getElementsByClassName("swiper-slide-active")[0].getElementsByClassName('summary')[0].innerHTML;
-    redirectLinkedinId = document.getElementsByClassName("swiper-slide-active")[0].getElementsByClassName('uid')[0].innerHTML;
+  $scope.lookDetail = function(index){
+    sharedData.friend = sharedData.matchedList[index];
+    // var tmpAvatarString = document.getElementsByClassName("swiper-slide-active")[0].getElementsByClassName("item-image")[0].getAttribute("style");
+    // redirectAvatar = tmpAvatarString.substring(tmpAvatarString.indexOf("('")+2,tmpAvatarString.indexOf("')"));
+    // redirectName = document.getElementsByClassName("swiper-slide-active")[0].getElementsByTagName('h2')[0].innerText;
+    // redirectHeadLine = document.getElementsByClassName("swiper-slide-active")[0].getElementsByTagName('h3')[0].innerText;
+    // redirectDistance = document.getElementsByClassName("swiper-slide-active")[0].getElementsByTagName('p')[0].innerText;
+    // redirectSummary = document.getElementsByClassName("swiper-slide-active")[0].getElementsByClassName('summary')[0].innerHTML;
+    // redirectLinkedinId = document.getElementsByClassName("swiper-slide-active")[0].getElementsByClassName('uid')[0].innerHTML;
     //window.location.href = redirectURL;
     $state.go('details');
   }
 });
 
-nameApp.controller('DetailsCtrl', function($scope, $state, $ionicHistory) {
+nameApp.controller('DetailsCtrl', function($scope, $state, $ionicHistory, sharedData) {
   $scope.goBack = function(){
     $ionicHistory.goBack();
   }
@@ -275,24 +314,107 @@ nameApp.controller('DetailsCtrl', function($scope, $state, $ionicHistory) {
     $ionicHistory.goBack();
   }
 
+  var curProfile = sharedData.profile;
+  var friend = sharedData.friend;
+
   // fill detailed page with person information
-  $scope.personDetail = {
-    pictureUrl: redirectAvatar,
-    formattedName: redirectName,
-    headline: redirectHeadLine,
-    distance: redirectDistance,
-    summary: redirectSummary
-  }
+  // $scope.personDetail = {
+  //   pictureUrl: redirectAvatar,
+  //   formattedName: redirectName,
+  //   headline: redirectHeadLine,
+  //   distance: redirectDistance,
+  //   summary: redirectSummary
+  // }
+
+  $scope.personDetail = friend;
 
   // send invitation
   document.getElementById("yeah-detail-btn").onclick = function() {
     $.ajax({
       method: 'POST',
-      url: SERVER_URL+'/api/user/'+profileLinkedinId+'/friends/'+redirectLinkedinId
+      url: SERVER_URL+'/api/user/'+profile.linkedinId+'/friends/'+friend.linkedinId
     }).then(function successCallback(response) {
       alert("invitation sent! :)");
     }, function errorCallback(response) {
       alert("Something goes wrong :( try again later!");
     });
   }
+});
+
+nameApp.controller('ChatCtrl', function($scope, $state, $ionicHistory, sharedData) {
+  var uid = $state.params.linkedinId;
+  var friendId = $state.params.friendLinkedinId;
+
+  if(!firebase.apps.length) {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyBst2CUBQYUJZZ8eEg-KhcgSi8L_P6tJVs",
+      authDomain: "lynking-33fb0.firebaseapp.com",
+      databaseURL: "https://lynking-33fb0.firebaseio.com",
+      storageBucket: "lynking-33fb0.appspot.com",
+      messagingSenderId: "489592513195"
+    };
+    firebase.initializeApp(config);
+
+    // inital auth
+    $.ajax({
+      method: 'POST',
+      url: 'http://localhost:8080'+'/api/user/'+uid+'/chatToken'
+    })
+    .then(function (res) {
+      $scope.auth = firebase.auth().signInWithCustomToken(res.chatToken.chatToken);
+
+      var dbRef = firebase.database().ref('chats/'+uid+'/'+friendId);
+      var dbRefCopy = firebase.database().ref('chats/'+friendId+'/'+uid);
+
+      dbRef.off();
+
+      dbRef.limitToLast(12).on('child_added', refreshMessage);
+      // dbRefCopy.limitToLast(12).on('child_added', refreshMessage);
+
+      $scope.dbRef = dbRef;
+      $scope.dbRefCopy = dbRefCopy;
+
+    })
+    .fail(function(err) {
+      alert(err.responseJSON.errorMessage);
+    });
+
+    // methods
+    $scope.sendMessage = function() {
+      var text = $scope.input.text;
+      if(text === null || text === '') {
+        return;
+      }
+
+      var curProfile = sharedData.profile;
+      var message = {
+        linkedinId: uid,
+        text: text,
+        photoUrl: curProfile.pictureUrl,
+        name: curProfile.formattedName,
+        // photoUrl: uid === profileLinkedinId ? profilePictureUrl : redirectAvatar,
+        // name: uid === profileLinkedinId ? profileName : redirectName,
+        timestamp: Date.now()
+      };
+
+      $scope.dbRef.push(message);
+      $scope.dbRefCopy.push(message);
+    }
+  }
+
+  // refresh view message
+  function refreshMessage(data) {
+      var val = data.val();
+      $scope.messages.push(val);
+      $scope.$apply();
+  }
+
+  $scope.messages = [];
+  $scope.input = {
+    text: ''
+  };
+
+  // $scope. = $state;
+  // debugger;
 });
